e137f2c0ae22c0b8ad7c987ea9e121f3b7acb87b
Daniel Ajadi <ajadidan@gmail.com>
2019-10-15 16:10:01 -0400

    MVP Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Navigation />
+
+    <Route exact path="/" component={Home} /> 
+    <Route exact path="/about-page" component={About} />
+    <Route exact path="/contact-page" component={Contact} />
   </div>
 );
 
